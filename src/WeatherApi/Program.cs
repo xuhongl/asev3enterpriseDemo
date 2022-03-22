8ab4a5358a2a5eaafeaae30f43f322528d9db7f4
hugogirard <hugo.girard@microsoft.com>
2022-03-22 11:22:05 -0400

    add health check

--- src/WeatherApi/Program.cs
+++ src/WeatherApi/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -29,6 +30,8 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+app.MapHealthChecks("/healthz");
 
 app.Run();
 
