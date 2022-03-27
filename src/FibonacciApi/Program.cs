b138838a9f1866be456e22ca34efa7a2697d913f
hugogirard <hugo.girard@microsoft.com>
2022-03-27 11:26:22 -0400

    add health check fibonnaci api

--- src/FibonacciApi/Program.cs
+++ src/FibonacciApi/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -26,5 +27,7 @@ app.MapGet("/api/fibonacci/getsequence", (int len) =>
     return sequences;
 })
 .WithName("GetSequence");
+
+app.MapHealthChecks("/healthz");
 
 app.Run();
